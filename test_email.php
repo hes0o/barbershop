54de0bec04cfe6143490caff6d2444148b3b707c
hes0o <hassanchawa30@gmail.com>
2025-06-15 22:27:26 +0300

    trying the email

--- test_email.php
+++ test_email.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/includes/email_helper.php';
 
 $email = new EmailHelper();
 $result = $email->send(
-    'bladex@customprojects.shawa.com.tr', // <-- Replace with your real email to receive the test
+    'hassanshawa30@gmail.com', // <-- Replace with your real email to receive the test
     'Test Email from BarberShop',
     '<h1>This is a test email</h1><p>If you see this, your SMTP is working!</p>',
     'This is a test email. If you see this, your SMTP is working!'
