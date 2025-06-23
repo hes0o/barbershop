a15f501ca28f10b70ee202d730c9161629d2419d
hes0o <hassanchawa30@gmail.com>
2025-06-23 13:53:54 +0300

    updates

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,6 @@ if (file_exists(__DIR__ . '/../.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }
-echo '<pre>'; print_r($_ENV); echo '</pre>'; exit;
 define('DB_HOST', $_ENV['DB_HOST'] ?? 'localhost');
 define('DB_USER', $_ENV['DB_USER'] ?? 'root');
 define('DB_PASS', $_ENV['DB_PASS'] ?? '');
