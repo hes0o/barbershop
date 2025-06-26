8e94095f45a51a6dece7a9468131418c29dcbfca
hes0o <hassanchawa30@gmail.com>
2025-06-26 16:23:19 +0300

    adding an error showing lines to the index page

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
-require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/includes/config.php';
 
 // If user is already logged in, redirect to appropriate dashboard
 if (isset($_SESSION['user_id'])) {
